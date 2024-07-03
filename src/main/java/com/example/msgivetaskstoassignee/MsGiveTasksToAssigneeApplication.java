db23e907c8b8853605ccba9e30026de6b5cec5a4
Baloghlan <qasimovbaloglan13@gmail.com>
2024-07-03 13:55:15 +0400

    spring boot initialization

---
+++ src/main/java/com/example/msgivetaskstoassignee/MsGiveTasksToAssigneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgivetaskstoassignee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGiveTasksToAssigneeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGiveTasksToAssigneeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msgivetaskstoassignee/MsGiveTasksToAssigneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgivetaskstoassignee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGiveTasksToAssigneeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
