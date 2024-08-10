b48e7d05a3b265120754b0623bad0349483c79b2
Baloghlan <qasimovbaloglan13@gmail.com>
2024-08-11 01:44:59 +0400

    fixed errors and comment security

--- src/main/java/com/example/msgivetaskstoassignee/config/WebSecuirtyConfig.java
+++ src/main/java/com/example/msgivetaskstoassignee/config/WebSecuirtyConfig.java
@@ -1,53 +1,53 @@
-package com.example.msgivetaskstoassignee.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecuirtyConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-
-                        .requestMatchers("/", "/home", "/telesales").hasAnyRole("ADMIN,USER")
-                        .requestMatchers("/tasks/**").hasRole("ADMIN")
-                        .requestMatchers("/telesales/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST,"/telesales").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                .logout(LogoutConfigurer::permitAll);
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("userpassword")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("adminpassword")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-}
+//package com.example.msgivetaskstoassignee.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecuirtyConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//
+//                        .requestMatchers("/", "/home", "/telesales").hasAnyRole("ADMIN,USER")
+//                        .requestMatchers("/tasks/**").hasRole("ADMIN")
+//                        .requestMatchers("/telesales/**").hasRole("USER")
+//                        .requestMatchers(HttpMethod.POST,"/telesales").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+//                .logout(LogoutConfigurer::permitAll);
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("userpassword")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("adminpassword")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+//
+//}
